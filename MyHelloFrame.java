1366bf143309d05c2b9609a3ba81aec2550c080b
Harui1537 <dbgkgygod123@naver.com>
2020-11-23 12:46:42 +0900

    AppNFrame

--- MyHelloFrame.java
+++ MyHelloFrame.java
@@ -1,33 +1,20 @@
-
+import javax.swing.*;
 /**
  * 여기에 MyHelloFrame 클래스 설명을 작성하십시오.
  * 
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
-public class MyHelloFrame
+public class MyHelloFrame extends JFrame
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * MyHelloFrame 클래스의 객체 생성자
-     */
-    public MyHelloFrame()
-    {
-        // 인스턴스 변수의 초기화
-        x = 0;
-    }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
+    public MyHelloFrame(){
+        this.setTitle("실습#4(2020.11.23)");
+        this.setSize(500,500);
+        
+        JPanel mp = new MyHelloPanelListener();
+        this.add(mp);
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
 }
